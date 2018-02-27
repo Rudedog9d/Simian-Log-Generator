20371544d00e1c6d806a90e3cf1a373b7ecbd6e1
Brodie Davis <bdavis9d@gmail.com>
2018-02-26 23:27:44 -0500

    basic form steup

---
+++ WCM Log Generator/Main.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WCM_Log_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtSourceDirectory = new System.Windows.Forms.TextBox();
+            this.DirectoryPicker = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutputDirectory = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // txtSourceDirectory
+            // 
+            this.txtSourceDirectory.Location = new System.Drawing.Point(107, 9);
+            this.txtSourceDirectory.Name = "txtSourceDirectory";
+            this.txtSourceDirectory.Size = new System.Drawing.Size(439, 20);
+            this.txtSourceDirectory.TabIndex = 0;
+            // 
+            // DirectoryPicker
+            // 
+            this.DirectoryPicker.FileName = "DirectoryPicker";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source Directrory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output Directory";
+            // 
+            // txtOutputDirectory
+            // 
+            this.txtOutputDirectory.Location = new System.Drawing.Point(107, 35);
+            this.txtOutputDirectory.Name = "txtOutputDirectory";
+            this.txtOutputDirectory.Size = new System.Drawing.Size(439, 20);
+            this.txtOutputDirectory.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 341);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutputDirectory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSourceDirectory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSourceDirectory;
+        private System.Windows.Forms.OpenFileDialog DirectoryPicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutputDirectory;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
